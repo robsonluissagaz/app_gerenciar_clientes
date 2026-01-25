d1a4a83ec1b64add1479a781f6e947a1ea883cc6
Robson Luis Sagaz <robsonluissagaz@gmail.com>
2026-01-25 01:02:07 -0300

    att

--- main.py
+++ main.py
@@ -7,11 +7,47 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from datetime import datetime
+import sqlite3
+
+
+#Setando o banco de dados(o própio aplicativo cria o arquivo caso ele não exista)
+#Tabela de clientes
+con = sqlite3.connect("app.db")
+cursor = con.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS clientes (
+    nome TEXT NOT NULL,
+    telefone TEXT
+)
+""")
+#Tabela de ordem de serviços
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS ordens_servico (
+    tipo TEXT,
+    endereco TEXT,
+    data TEXT,
+    telefone TEXT,
+    pago TEXT,
+    descricao TEXT
+)
+""")
 
 
 #Passando o gerenciador
 class MeuGerenciador(ScreenManager):
     pass
+
+
+#Função para cadastrar um novo cliente no banco de dados
+def cadastrar_cliente(nome, telefone, email):
+    con = sqlite3.connect("app.db")
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO clientes (nome, telefone) VALUES (?, ?)",
+        (nome, telefone)
+    )
+    con.commit()
+    con.close()
 
 
 #Tela inicial
@@ -21,17 +57,23 @@ class TelaInicio(Screen):
 
 
 #Tela de gerar serviços
+class CadastroCliente(Screen):
+    def nada():
+        return
+
+
 class GerarServico(Screen):
     def nada():
         return
 
 
+#Configuração de inicio do aplicativo
 GUI = Builder.load_file('tela.kv')
 class MeuAplicativo(App):
     def build(self):
         return GUI
 
-
+    #Fechar o aplicativo ao pressionar a tecla de voltar do android
     def fechar_aplicativo(self):
         App.get_running_app().stop()
 
